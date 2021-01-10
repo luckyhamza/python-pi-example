4dfe661183265d5ce88c5a8d454182c710e28ecd
lucky@pointybracket.net <luckyhamza@pointybracket.net>
2021-01-10 19:06:35 +0500

    newbranch

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 
 def main(msg):
-<<<<<<< HEAD
-#adding 2nd comment to this file
-=======
-    #print a message
-    #hy john
-
->>>>>>> e55b9000081333b77a80c08ccdf6d2e9ce86ffd2
+#no longer need comment
     print(msg)
 
 
+def secondary():
+     print('new function')
+     
+     
 main('hi john')
